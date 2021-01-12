f9cb397d0c70193f1d9ccfea8be3de0c5061c3a9
forw <allanzyne@outlook.com>
2021-01-12 11:00:43 +0800

    2021-01-12

---
+++ fish/config.wsl.fish
@@ -0,0 +1,5 @@
+# clear windows path
+set -x PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+
+# VcXsrv
+set -x DISPLAY 172.17.16.1:0
