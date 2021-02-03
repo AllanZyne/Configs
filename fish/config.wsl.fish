86b04b1e0f9c9bdcc84d90ac6d2a7cd1d0a5caab
forw <allanzyne@outlook.com>
2021-02-03 11:16:56 +0800

    2021-02-03

--- fish/config.wsl.fish
+++ fish/config.wsl.fish
@@ -2,4 +2,7 @@
 set -x PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 
 # VcXsrv
-set -x DISPLAY 172.17.16.1:0
+set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+
+# Pulse(Audio)
+#set -x PULSE_SERVER (cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
