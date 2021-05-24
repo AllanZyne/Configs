f19f859f6e7124181a00e22039bb17b09ee491b7
forw <allanzyne@outlook.com>
2021-05-24 10:05:22 +0800

    2021-05-24

--- fish/config.wsl.fish
+++ fish/config.wsl.fish
@@ -1,5 +1,6 @@
 # clear windows path
 set -x PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
+set -ax PATH (wslpath -a "C:\Users\allan\AppData\Local\Programs\Microsoft VS Code\bin")
 
 # VcXsrv
 set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
